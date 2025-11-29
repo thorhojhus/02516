d9b2ec013c4cdbb25dd749c1d27759f2f1e97bd6
Oliver Hansen <hiffidrengen@gmail.com>
2025-11-29 14:00:23 +0100

    Add ResNet pothole Detector model

--- project4/models/object_detector.py
+++ project4/models/object_detector.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 # Object Detecting Model
-
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -57,3 +57,23 @@ class CNN(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.fully_connected(x)
         return x
+
+class PotholeResNet(nn.Module):
+    def __init__(self):
+        super(PotholeResNet, self).__init__()
+        self.base_model = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+
+        for param in self.base_model.parameters():
+            param.requires_grad = False  # Freeze base model parameters
+
+        self.base_model.fc = nn.Linear(self.base_model.fc.in_features, 2)  # background & potholes
+
+    def forward(self, x):
+        x = self.base_model(x)
+        return x # logits
+
+if __name__ == "__main__":
+    model = PotholeResNet()
+    sample_input = torch.randn(4, 3, 224, 224)  # batch of 4 RGB images of size 224x224
+    output = model(sample_input)
+    print(output.shape)  # Expected output shape: [4, 2]
